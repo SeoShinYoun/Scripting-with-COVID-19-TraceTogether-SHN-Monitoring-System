7e2fb83a62c467cd55c78fd00a04f80b12524705
Cheng Ann Phua <s10203193@connect.np.edu.sg>
2021-01-20 16:22:44 +0800

    Add project files.

---
+++ Prg_Assg_CASY/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prg_Assg_CASY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
