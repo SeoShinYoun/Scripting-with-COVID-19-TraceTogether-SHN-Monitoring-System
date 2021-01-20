76cb35da615e5f83571a2646bd24f30602731bb0
Cheng Ann Phua <s10203193@connect.np.edu.sg>
2021-01-20 20:08:11 +0800

    Update

--- Prg_Assg_CASY/BusinessLocation.cs
+++ Prg_Assg_CASY/BusinessLocation.cs
@@ -6,5 +6,58 @@ namespace Prg_Assg_CASY
 {
     class BusinessLocation
     {
+        private string businessName;
+        private string branchCode;
+        private int maximumCapacity;
+        private int visitorsNow; 
+
+        public string BusinessName
+        {
+            get { return businessName; }
+            set { businessName = value; }
+        }
+
+        public string BranchCode
+        {
+            get { return branchCode; }
+            set { branchCode = value; }
+        }
+
+        public int MaximumCapacity
+        {
+            get { return maximumCapacity; }
+            set { maximumCapacity = value; }
+        }
+
+        public int VisitorsNow
+        {
+            get { return visitorsNow; }
+            set { visitorsNow = value; }
+        }
+
+        public BusinessLocation() { }
+
+        public BusinessLocation(string n, string c, int mc)
+        {
+            businessName = n;
+            branchCode = c;
+            maximumCapacity = mc;
+        }
+
+        public bool IsFull()
+        {
+            if (VisitorsNow == MaximumCapacity)
+            {
+                return true;
+            }
+            
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
     }
 }
