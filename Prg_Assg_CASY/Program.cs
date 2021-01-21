9cb6277a85a85d1f958d4632a170ad092a89509b
Cheng Ann Phua <s10203193@connect.np.edu.sg>
2021-01-21 21:42:04 +0800

    1

--- Prg_Assg_CASY/Program.cs
+++ Prg_Assg_CASY/Program.cs
@@ -6,10 +6,7 @@ namespace Prg_Assg_CASY
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Testing");
-            Console.WriteLine("hi");
-            Console.WriteLine("hilko");
+
         }
     }
 }
---
+++ Prg_Assg_CASY/SafeEntry.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prg_Assg_CASY
+{
+    class SafeEntry
+    {
+        private DateTime checkIn;
+        private DateTime checkOut;
+        private BusinessLocation location;
+
+        public DateTime CheckIn
+        {
+            get { return checkIn; }
+            set { checkIn = value; }
+        }
+
+        public DateTime CheckOut
+        { 
+            get { return checkOut; }
+            set { checkOut = value; }
+        }
+
+        public BusinessLocation Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public SafeEntry() { }
+
+        public SafeEntry(DateTime In, BusinessLocation L )
+        {
+            CheckIn = In;
+            Location = L;
+        }
+
+        public virtual void PerformCheckOut()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
