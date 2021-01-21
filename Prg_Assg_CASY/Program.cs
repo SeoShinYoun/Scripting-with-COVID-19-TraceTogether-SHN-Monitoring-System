74bf164d60a25884067aa5e93d6ae20aa3837f88
Seo Shin Youn <s10205100@connect.np.edu.sg>
2021-01-21 13:30:00 +0800

    Loaded SHNFacility API data into shnfacility List

--- Prg_Assg_CASY/Program.cs
+++ Prg_Assg_CASY/Program.cs
@@ -10,7 +10,21 @@ namespace Prg_Assg_CASY
     {
         static void Main(string[] args)
         {
-            
+            List<SHNFacility> shnfacilityList = new List<SHNFacility>();
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://covidmonitoringapiprg2.azurewebsites.net");
+                Task<HttpResponseMessage> responseTask = client.GetAsync("/facility");
+                responseTask.Wait();
+                HttpResponseMessage result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    Task<string> readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+                    string data = readTask.Result;
+                    shnfacilityList = JsonConvert.DeserializeObject<List<SHNFacility>>(data);
+                }
+            }
         }
     }
 }
