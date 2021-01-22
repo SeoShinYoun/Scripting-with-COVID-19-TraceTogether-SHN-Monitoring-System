7d1a934ac1e631ebcdde308b330011141cc72101
Seo Shin Youn <s10205100@connect.np.edu.sg>
2021-01-22 13:15:02 +0800

    1) Added comments for For Student Number, Name, Group. 2)Implemeted SHN Duration method from Travel Entry Class

--- Prg_Assg_CASY/Program.cs
+++ Prg_Assg_CASY/Program.cs
@@ -4,12 +4,19 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json;
 
+//============================================================
+// Student Number : S10205100, S10203193
+// Student Name : Seo Shin Youn, Phua Cheng Ann
+// Module Group : T09 //============================================================
+
 namespace Prg_Assg_CASY
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+
             // Calling SHNFacility class API (Basic Feature 2)
             List<SHNFacility> shnfacilityList = new List<SHNFacility>();
             using (HttpClient client = new HttpClient())
